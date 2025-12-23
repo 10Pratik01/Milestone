113f6eacf0af0b34c04c278dc37fcb321bcff6ce
10Pratik01 <using0this@gmail.com>
2025-12-24 00:48:52 +0530

    feat- removed dist from github

--- server/dist/routes/projectRoutes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { getProjects } from "../controllers/projectControllers.js";
-const router = Router();
-router.get("/", getProjects);
-export default router;
